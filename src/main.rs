a3e36e6dc0e39151aa76051daf6e381964be8cfb
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-05-23 15:58:00 +0200

    serve all kinds of statics

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,34 @@
-use axum::{response::Html, routing::get, Router};
+use axum::{
+    body::{self, Empty, Full},
+    extract::Path,
+    http::{header, HeaderValue, Response, StatusCode},
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
+use include_dir::{include_dir, Dir};
+
+// Static file serving.
+static STATICS_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/static/");
+async fn serve_statics(Path(path): Path<String>) -> impl IntoResponse {
+    let path = path.trim_start_matches('/');
+    let mime_type = mime_guess::from_path(path).first_or_text_plain();
+
+    match STATICS_DIR.get_file(path) {
+        None => Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body(body::boxed(Empty::new()))
+            .unwrap(),
+        Some(file) => Response::builder()
+            .status(StatusCode::OK)
+            .header(
+                header::CONTENT_TYPE,
+                HeaderValue::from_str(mime_type.as_ref()).unwrap(),
+            )
+            .body(body::boxed(Full::from(file.contents())))
+            .unwrap(),
+    }
+}
 
 async fn serve_index() -> Html<&'static str> {
     Html(include_str!("../templates/index.html"))
@@ -6,7 +36,9 @@ async fn serve_index() -> Html<&'static str> {
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(serve_index));
+    let app = Router::new()
+        .route("/", get(serve_index))
+        .route("/static/*path", get(serve_statics));
     axum::Server::bind(
         &"127.0.0.1:8080"
             .parse()
