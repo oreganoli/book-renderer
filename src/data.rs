79f08e884e2520c2e7bfe0255de23fc45b5935c0
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-05-23 15:47:09 +0200

    added more detailed type for templating

--- src/data.rs
+++ src/data.rs
@@ -13,3 +13,40 @@ pub struct BookData {
     /// URL pointing at the store page to buy the book.
     shop_url: String,
 }
+
+/// Struct for displaying the full book data.
+pub struct Book {
+    data: BookData,
+    description: String,
+    available: bool,
+}
+
+impl From<BookData> for Book {
+    fn from(data: BookData) -> Self {
+        let description = if data.price == -1.0 || data.year == -1 || data.pages == -1 {
+            "-".to_string()
+        } else {
+            let series_string = if data.series.is_empty() {
+                "".to_string()
+            } else {
+                format!(" (part of the {} series)", &data.series)
+            };
+            format!(
+                "{}{} by {}, {} {}, {} pages, {} cover",
+                &data.title,
+                series_string,
+                &data.author,
+                &data.publisher,
+                &data.year,
+                &data.pages,
+                &data.cover
+            )
+        };
+        let available = data.price != -1.0;
+        Self {
+            data,
+            description,
+            available,
+        }
+    }
+}
