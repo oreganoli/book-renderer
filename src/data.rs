107e30ffc779f98dd4cfe884a84aac6cfef410ec
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-05-23 17:33:58 +0200

    revamp description generation

--- src/data.rs
+++ src/data.rs
@@ -24,37 +24,56 @@ pub struct Book {
     pub data: BookData,
     pub description: String,
     pub available: bool,
+    pub link: Option<String>,
 }
-
+// Helper function for possibly missing fields.
+fn hyphenize_empty(string: &str) -> String {
+    if string.is_empty() {
+        "-".to_string()
+    } else {
+        string.to_owned()
+    }
+}
 impl From<BookData> for Book {
     fn from(data: BookData) -> Self {
-        let (available, description) = if data.price == -1.0 || data.year == -1 || data.pages == -1
-        {
-            (false, "-".to_string())
-        } else {
-            let series_string = if data.series.is_empty() {
-                "".to_string()
+        let available = data.price != -1.0;
+        let description = {
+            let price_text = if data.price == -1.0 {
+                "-".to_string()
             } else {
-                format!(" (part of the {} series)", &data.series)
+                data.price.to_string().replace(".", ",") // European decimals
             };
-            (
-                true,
-                format!(
-                    "{}{} by {}, {} {}, {} pages, {} cover",
-                    &data.title,
-                    series_string,
-                    &data.author,
-                    &data.publisher,
-                    &data.year,
-                    &data.pages,
-                    &data.cover
-                ),
+            let year_text = if data.year == -1 {
+                "-".to_string()
+            } else {
+                data.year.to_string()
+            };
+            let pages_text = if data.pages == -1 {
+                "-".to_string()
+            } else {
+                data.pages.to_string()
+            };
+
+            format!(
+                "Autor: {}, cena: {}, wydawca: {}, rok wydania: {}, liczba stron: {}, okładka: {}",
+                hyphenize_empty(&data.author),
+                price_text,
+                hyphenize_empty(&data.publisher),
+                year_text,
+                pages_text,
+                hyphenize_empty(&data.cover)
             )
+        };
+        let link = if data.shop_url.is_empty() {
+            None
+        } else {
+            Some(data.shop_url.to_owned())
         };
         Self {
             data,
             description,
             available,
+            link,
         }
     }
 }
