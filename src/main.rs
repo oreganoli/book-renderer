4f8baf4a181229879cae816f381215889a2c192c
oreganoli <3611916+oreganoli@users.noreply.github.com>
2022-05-23 15:35:38 +0200

    serves index.html

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Html, routing::get, Router};
+
+async fn serve_index() -> Html<&'static str> {
+    Html(include_str!("../templates/index.html"))
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(serve_index));
+    axum::Server::bind(
+        &"127.0.0.1:8080"
+            .parse()
+            .expect("could not bind to port 8080"),
+    )
+    .serve(app.into_make_service())
+    .await
+    .unwrap();
 }
